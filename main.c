c0d43be14ed499b97138c2f22e5737d3007e0801
Franz <franzbischoff@gmail.com>
2015-10-06 02:40:04 +0100

    Final release

--- main.c
+++ main.c
@@ -7,19 +7,19 @@
 
 /*
  * O Problema
- * Dada um conjunto de N intervalos de n�meros, a tua tarefa � descobrir, para cada intervalo, qual o(s)
- * d�gito(s) que aparece mais vezes, bem como quantas vezes ele aparece.
+ * Dada um conjunto de N intervalos de numeros, a tua tarefa e descobrir, para cada intervalo, qual o(s)
+ * digito(s) que aparece mais vezes, bem como quantas vezes ele aparece.
  *
  * Input
- * Na primeira linha vem um �nico n�mero inteiro N, indicando a quantidade de intervalos a considerar.
- * Nas N linhas seguintes vem um par de n�meros Ai Bi (separados por um espa�o), indicando que i-�simo
- * intervalo de n�meros a considerar � [A,B] (ou seja, os n�meros de A a B, inclusive).
+ * Na primeira linha vem um unico numero inteiro N, indicando a quantidade de intervalos a considerar.
+ * Nas N linhas seguintes vem um par de numeros Ai Bi (separados por um espaco), indicando que i-esimo
+ * intervalo de numeros a considerar e [A,B] (ou seja, os numeros de A a B, inclusive).
  *
  * Output
- * O output deve ser constitu�do por N linhas, uma para cada um dos intervalos do input. Cada uma destas
- * linhas deve come�ar por ter um n�mero inteiro indicando quantas vezes aparece o d�gito mais frequente,
- * seguida de uma lista (por ordem crescente) dos d�gitos que aparecem com essa frequ�ncia m�xima. Todos
- * os n�meros de uma mesma linha devem ser separados por um �nico espa�o.
+ * O output deve ser constituido por N linhas, uma para cada um dos intervalos do input. Cada uma destas
+ * linhas deve comecar por ter um numero inteiro indicando quantas vezes aparece o digito mais frequente,
+ * seguida de uma lista (por ordem crescente) dos digitos que aparecem com essa frequencia maxima. Todos
+ * os numeros de uma mesma linha devem ser separados por um unico espaco.
  *
  */
 
@@ -29,7 +29,7 @@
 #include <stdint.h>
 
 /*
- *  0 1 2 3 4 5 6 7 8 9 // 1 u
+ * 0 1 2 3 4 5 6 7 8 9 // 1 u
  * 10 11 12 13 14 15 16 17 18 19 // 1 u 10 Ed
  * 100 101 102 103 104 105 106 107 108 109 // 10 Eu 10 Ed 100 c
  * 1000 1001 1002 1003 1004 1005 1006 1007 1008 1009 // 100 Eu 100 Ed 100 Ec 1000 m
@@ -37,21 +37,22 @@
  * */
 
 void calc_freq_base(uint32_t a, uint32_t b);
+uint64_t quick_pow10(int32_t n);
 void calc_freq();
-void print_freq();
 void print_most_frequent();
 void print_help();
 
 uint32_t count[10];
 
-int main(int argc, char** argv)
+int32_t main(int32_t argc, char** argv)
 {
     uint32_t a = 0;
     uint32_t b = 0;
     uint32_t i, j, k;
-    int intervals = 0;
+    int32_t intervals = 0;
     char arg;
     uint8_t interactive = 0;
+    uint8_t simple = 0;
 
     for (i = 1; i < argc; i++)
     {
@@ -72,6 +73,9 @@ int main(int argc, char** argv)
                     print_help();
                     return (EXIT_SUCCESS);
                     break;
+                case 's':
+                    simple = 1;
+                    break;
                 default:
                     break;
                 }
@@ -90,8 +94,11 @@ int main(int argc, char** argv)
 
         fscanf(stdin, "%u%u", &a, &b);
 
-        // calc_freq_base(a, b);
-        calc_freq(a, b);
+        if (simple)
+            calc_freq_base(a, b);
+        else
+            calc_freq(a, b);
+        
         print_most_frequent();
     }
 
@@ -146,7 +153,8 @@ void calc_freq_base(uint32_t a, uint32_t b)
 
 void calc_freq(uint32_t a, uint32_t b)
 {
-    int32_t m, n, magnitude, i, j, k;
+    uint64_t magnitude;
+    int32_t m, n, i, j, k;
 
     // initialize counters
     for (i = 0; i < 10; i++)
@@ -223,16 +231,6 @@ void calc_freq(uint32_t a, uint32_t b)
     }
 }
 
-void print_freq()
-{
-    int i;
-
-    for (i = 0; i < 10; i++)
-    {
-        printf("%d = %d\n", i, count[i]);
-    }
-}
-
 void print_help()
 {
     printf("IMPERIAL FLEET PROBLEM - Introducao a informatica\n\n");
@@ -245,16 +243,21 @@ void print_help()
     printf("\tFORMAT:\n\t\tThe first line holds an integer 'N' with the number of intervals do consider. This number must be 1 <= N <= 1,000.\n");
     printf("\t\tThe following lines are composed by two integers 'A' and 'B'. These numbers must be 1 <= A <= B <= 1,000,000,000.\n\n");
 
-    printf("\tOUTPUT:\n\t\tThe output is composed of N lines. The first number tells frequency and the following numbers the digits that are most frequent.\n\n");
+    printf("\tOUTPUT:\n\t\tThe output is composed of N lines. The first number tells the frequency and the following numbers the digits that are most frequent.\n\n");
+
+    printf("OPTIONS\n\n");
+    printf("\t-h This help.\n\n");
+    printf("\t-i Interactive mode.\n\n");
+    printf("\t-s Simple, not optimized, algorithm.\n\n");
 
     printf("AUTHOR\n\n");
     printf("\tFrancisco Bischoff.\n\n");
 }
 
-int quick_pow10(int n)
+uint64_t quick_pow10(int32_t n)
 {
-    static int pow10[10] = {
-        1, 10, 100, 1000, 10000, 100000, 1000000, 10000000, 100000000, 1000000000
+    static uint64_t pow10[11] = {
+        1, 10, 100, 1000, 10000, 100000, 1000000, 10000000, 100000000, 1000000000, 10000000000
     };
 
     if (n < 0)
